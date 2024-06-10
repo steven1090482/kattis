8531b1cb8918c6f073ad6ff651e1f55c04eb0be6
steven1090482 <160751793+steven1090482@users.noreply.github.com>
2024-06-10 13:13:59 +0800

    TOJ 519 - 山彦.cpp

---
+++ TOJ 519 - 山彦.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+	int num1,num2;
+	cin >> num1;
+	cin >> num2;
+	cout <<"Do you want to say "<<num1<<" and "<<num2<<" ??\n";
+	return 0;
+}
