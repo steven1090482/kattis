eb58973c5f77aec42f1cc5c9808413c7e4dfaad3
steven1090482 <160751793+steven1090482@users.noreply.github.com>
2024-06-10 13:40:34 +0800

    TOJ 524 - 小芬的難題.cpp

---
+++ TOJ 524 - 小芬的難題.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+	int num1,num2;
+	cin >> num1;
+	cin >> num2;
+	int a=(num1+num2)/2;
+	int b=(num1-num2)/2;
+	cout << "a: "<<a<<", b: "<<b<<"\n";
+	return 0;
+}
