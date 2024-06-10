05fd57ecec820d9c881865c8714364ee706c0fe0
steven1090482 <160751793+steven1090482@users.noreply.github.com>
2024-06-10 13:33:57 +0800

    Kattis jackolanternjuxtaposition - Jack-O’-Lantern Juxtaposition.cpp

---
+++ Kattis jackolanternjuxtaposition - Jack-O’-Lantern Juxtaposition.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+	int num1,num2,num3;
+	cin >> num1;
+	cin >> num2;
+	cin >> num3;
+	int number=num1*num2*num3;
+	cout <<number<<"\n";
+	return 0;
+}
